5b27def2327b354640f26b3c24935d65bb7bcbc4
Benish Sarinelli <bsarinelli@gmail.com>
2019-04-12 11:40:27 -0700

    adding security group

--- main.tf
+++ main.tf
@@ -1,10 +1,36 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_instance" "bbzexample" {
   ami		= "ami-40d28157"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
   tags {
     Name = "terraform-single-example"
-
   }  
 }
+
+resource "aws_security_group" "instance" {
+  name = "terraform-single-example"
+
+  ingress {
+    from_port = 8080
+    to_port   = 8080
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "public_ip" {
+  value = "${aws_instance.bbzexample.public_ip}"
+}
+
 
